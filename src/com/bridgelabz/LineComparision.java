bc5206949a3df1cf33775ce56a63539721221090
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-05 11:21:24 +0530

    Added: Length of Line

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -3,6 +3,12 @@ package com.bridgelabz;
 public class LineComparision {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparision Computation");
+        int x1 = 5;
+        int x2 = 7;
+        int y1 = 8;
+        int y2 = 89;
+
+        double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println(lengthOfLine1);
     }
 }
